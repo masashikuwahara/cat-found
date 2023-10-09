95e06dc4b332b46c4045d7d4b8a0d7bf33630331
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-10-09 21:24:48 +0900

    catsコントローラー作成

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,15 +1,24 @@
 class CatsController < ApplicationController
 
   def index
+    @cats = Cat.all
   end
 
   def show
+    @cat = Cat.find(params[:id])
   end
 
   def new
+    @cat = Cat.new
   end
 
   def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to @cat
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,4 +30,8 @@ class CatsController < ApplicationController
   def destroy
   end
   
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :place, :feature, :comment, :img1)
+  end
 end
